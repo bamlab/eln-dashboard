ed6068beea430e219b412978a4f691bc141777c9
Sammyt <sammy.teillet@gmail.com>
2018-11-27 16:06:47 +0100

    chore(): Add colors theme

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  mainColor: "#026AB5",
+  accentColor: "#029FE3",
+  secondaryColor: "#A1DAF7",
+  tertiaryColor: "#002677",
+  successColor: "#65A124",
+  errorColor: "#D0021B",
+  mainFontColor: "#3C3C3C",
+  secondaryFontColor: "#AAACAE",
+  tertiaryFontColor: "#99C3E1"
+};
