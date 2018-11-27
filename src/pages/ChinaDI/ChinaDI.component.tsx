a57947b157c8af109b13e19687f9a59b2a31841b
PierreCapo <pierrec@bam.tech>
2018-11-27 18:07:42 +0100

    :sparkle: Add cards

--- src/pages/ChinaDI/ChinaDI.component.tsx
+++ src/pages/ChinaDI/ChinaDI.component.tsx
@@ -1,12 +1,47 @@
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 import * as React from "react";
 import { ColumnChart } from "../../charts/ColumnChart";
 import { LineChart } from "../../charts/LineChart";
 import { LineColumnChart } from "../../charts/LineColumnChart";
 
-export const ChinaDI = () => (
-  <div>
-    <ColumnChart />
-    <LineChart />
-    <LineColumnChart />
-  </div>
-);
+const styles = {
+  root: {
+    padding: 24
+  }
+};
+const ChinaDIPage = (props: any) => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container={true} spacing={24}>
+        <Grid item={true} xs={12}>
+          <Card>
+            <CardContent>
+              <ColumnChart />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item={true} xs={12}>
+          <Card>
+            <CardContent>
+              <LineChart />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item={true} xs={12}>
+          <Card>
+            <CardContent>
+              <LineColumnChart />{" "}
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export const ChinaDI = withStyles(styles)(ChinaDIPage);
