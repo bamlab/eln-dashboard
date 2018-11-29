a756dfbcfaef2449bf83facfe6c6150b66e862c3
Sammyt <sammy.teillet@gmail.com>
2018-11-29 18:30:38 +0100

    feat(): Display real data China DC Trade flow

--- src/charts/LineColumnChart.tsx
+++ src/charts/LineColumnChart.tsx
@@ -3,30 +3,22 @@ import HighchartsReact from "highcharts-react-official";
 const Data = require("highcharts/modules/data");
 import * as Luxon from "luxon";
 import * as React from "react";
-import { json } from "../data/model_prediction_template";
+import { json } from "../data/ChinaDC-TradeFlow";
 
 Data(Highcharts);
 
-const serieWithTimestamp = json.map(serie => {
+const totalInvCoverage = json.map(serie => {
   const luxonDate = Luxon.DateTime.fromFormat(serie.Date, "M/d/yyyy");
-  return [luxonDate.toMillis(), serie.FO_CL_EC_BD / 80] as [number, number];
+  return [luxonDate.toMillis(), serie.total_inv_coverage] as [number, number];
 });
 
-const serie2WithTimestamp = json.map(serie => {
+const totalOfftake = json.map(serie => {
   const luxonDate = Luxon.DateTime.fromFormat(serie.Date, "M/d/yyyy");
-  const isNegative: number = Math.random() >= 0.5 ? 1 : -1;
-  return [luxonDate.toMillis(), (isNegative * serie.GU_IL_EC_KA) / 80] as [
-    number,
-    number
-  ];
+  return [luxonDate.toMillis(), serie.total_offtake] as [number, number];
 });
-const serie3WithTimestamp = json.map(serie => {
+const totalSGOPSellin = json.map(serie => {
   const luxonDate = Luxon.DateTime.fromFormat(serie.Date, "M/d/yyyy");
-  const isNegative: number = Math.random() >= 0.5 ? 1 : -1;
-  return [luxonDate.toMillis(), (isNegative * serie.IF_IL_EC_BD) / 80] as [
-    number,
-    number
-  ];
+  return [luxonDate.toMillis(), serie.total_SGOP_sellin] as [number, number];
 });
 
 const options: any = {
@@ -57,7 +49,7 @@ const options: any = {
     ],
     title: null,
     labels: {
-      format: "{value}%"
+      format: "{value}"
     }
   },
   tooltip: {
@@ -68,19 +60,22 @@ const options: any = {
   },
   series: [
     {
-      data: serie2WithTimestamp,
+      data: totalInvCoverage,
       type: "column",
-      color: "#A1DAF7"
+      color: "#A1DAF7",
+      name: "Total INV Coverage"
     },
     {
-      data: serie3WithTimestamp,
-      type: "column",
-      color: "#026AB5"
-    },
-    {
-      data: serieWithTimestamp,
+      data: totalSGOPSellin,
       type: "line",
-      color: "#65A124"
+      color: "#026AB5",
+      name: "Total SGOP Sellin"
+    },
+    {
+      data: totalOfftake,
+      type: "line",
+      color: "#65A124",
+      name: "Total Offtake"
     }
   ]
 };
--- src/pages/SummaryCurrentForecast/SummaryCurrentForecast_ChinaDC.component.tsx
+++ src/pages/SummaryCurrentForecast/SummaryCurrentForecast_ChinaDC.component.tsx
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import * as React from "react";
+import { LineColumnChart } from "src/charts/LineColumnChart";
 import { CustomTable } from "src/table/Table";
 
 const styles = {
@@ -65,6 +66,7 @@ const SummaryCurrentForecastComponent = (props: any) => {
                 Trade flow DC
               </Typography>
             </CardContent>
+            <LineColumnChart />
           </Card>
         </Grid>
         <Grid item={true} xs={12}>
