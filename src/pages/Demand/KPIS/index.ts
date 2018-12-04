27deb52cfa72be173d96e76c3b5dbeda0cb7fb2b
PierreCapo <pierrec@bam.tech>
2018-12-04 11:23:05 +0100

    :sparkle: Create new pages

---
+++ src/pages/Demand/KPIS/index.ts
@@ -0,0 +1,5 @@
+export { KPISChinaDC } from "./KPIS_ChinaDC.component";
+export { KPISChinaDI } from "./KPIS_ChinaDI.component";
+export { KPISInternationalDI } from "./KPIS_InternationalDI.component";
+export { KPISInternationalEIB } from "./KPIS_InternationalEIB.component";
+export { KPISInternationalIL } from "./KPIS_InternationalIL.component";
