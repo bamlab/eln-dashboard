7920087591c7080b0e71c2151a1211d672562cf9
Sammyt <sammy.teillet@gmail.com>
2018-12-04 10:49:24 +0100

    refactor(): Remove material ui warning

--- src/table/TableCell.tsx
+++ src/table/TableCell.tsx
@@ -3,6 +3,5 @@ import { colors } from "src/theme";
 
 export const TableCell = withStyles({
   paddingDense: { padding: 6, borderWidth: 0 },
-  header: { fontWeight: "bold", color: colors.mainColor },
   footer: { fontWeight: "bold", color: colors.mainColor }
 })(NativeTableCell);
