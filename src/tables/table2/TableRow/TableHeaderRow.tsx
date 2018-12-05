753cca9e3d3f6f3e19641b25f444ea57290b9d02
Sammyt <sammy.teillet@gmail.com>
2018-12-05 10:30:47 +0100

    deadcode(): remove useless tables

--- src/tables/table2/TableRow/TableHeaderRow.tsx
+++
@@ -1,5 +0,0 @@
-import { TableRow as NativeTableRow, withStyles } from "@material-ui/core";
-
-export const TableHeaderRow = withStyles({ root: { height: 40 } })(
-  NativeTableRow
-);
--- src/tables/table2/TableRow/TableRow.tsx
+++
@@ -1,3 +0,0 @@
-import { TableRow as NativeTableRow, withStyles } from "@material-ui/core";
-
-export const TableRow = withStyles({ root: { height: 20 } })(NativeTableRow);
