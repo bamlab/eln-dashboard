08a5a7676453a022029a31ca707bdb1edbfb39bf
PierreCapo <pierre.caporossi@gadz.org>
2018-12-10 17:07:01 +0100

    :sparkle: Add graph

--- src/pages/Category/SummaryActual/SummaryActual_Tailored.component.tsx
+++ src/pages/Category/SummaryActual/SummaryActual_Tailored.component.tsx
@@ -5,6 +5,7 @@ import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 import { HeatMap } from "src/charts/HeatMap";
+import { ColumnLineChart } from "src/charts/TradedFlowChart";
 import { CategoryHistoricalDemandContainer } from "src/components/CategoryHistoricalDemandContainer";
 import { SelectorOutlined } from "src/components/SelectorOutlined.component";
 
@@ -15,6 +16,11 @@ const styles = {
   font: {
     fontWeight: 700,
     fontSize: 16
+  },
+  subTitle: {
+    fontWeight: 700,
+    fontSize: 14,
+    marginTop: 16
   }
 };
 
@@ -52,6 +58,58 @@ const SummaryActual = (props: any) => {
                 </div>
               </div>
               <CategoryHistoricalDemandContainer />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item={true} xs={12}>
+          <Card>
+            <CardContent>
+              <Typography gutterBottom={true} className={classes.font}>
+                How are the demand change with baby pool evolution?
+              </Typography>
+              <Typography color="primary" className={classes.subTitle}>
+                Baby Pool (mn)
+              </Typography>
+              <ColumnLineChart
+                isStacked={false}
+                range="IMF total - summary actual & future baby pool mn!A:D"
+                customOptions={{
+                  yAxis: {
+                    gridLineWidth: 0,
+                    title: null,
+                    labels: {
+                      formatter() {
+                        const self: any = this as any;
+                        return `${Math.floor(self.value)}`;
+                      }
+                    }
+                  },
+                  tooltip: {
+                    formatter() {
+                      const self: any = this as any;
+                      return `${Math.floor(self.y)}`;
+                    }
+                  }
+                }}
+              />
+              <Typography color="primary" className={classes.subTitle}>
+                IMF Demand ('000 ton)
+              </Typography>
+              <ColumnLineChart
+                range="IMF total - summary actual & future baby pool ton!A:D"
+                customOptions={{
+                  legend: { enabled: false },
+                  yAxis: {
+                    gridLineWidth: 0,
+                    title: null,
+                    labels: {
+                      formatter() {
+                        return ``;
+                      }
+                    }
+                  }
+                }}
+              />
             </CardContent>
           </Card>
         </Grid>
